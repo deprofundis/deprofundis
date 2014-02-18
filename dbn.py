bd213534426009a7c79063a2f2cd18607a01c52b
David Haber <david.haber90@gmail.com>
2014-02-18 17:43:56 +0000

    First outline of DBN implementation - a few functions still to be implemented

--- dbn.py
+++ dbn.py
@@ -1,3 +1,5 @@
+from utils import grab_minibatch
+
 # TODO
 #
 # DBN with L layers
@@ -16,10 +18,11 @@
 
 class DBN(Network):
 
-    def __init__(self, lrate, momentum, wcost, layer_units, n_epochs=100000, k=1):
+    def __init__(self, lrate, momentum, wcost, layer_units, n_in_minibatch=100, n_epochs=100000, k=1):
         self.lrate = lrate
         self.momentum = momentum
         self.wcost = wcost
+        self.n_in_minibatch = n_in_minibatch
         self.n_epochs = n_epochs
 
         assert(isinstance(layer_units, list) and len(layer_units) > 2)
@@ -29,10 +32,10 @@ class DBN(Network):
         self.k = k
 
         # create rbms
-        self.n_layers = len(layer_units) - 1
+        self.n_layers = len(layer_units)
         self.rbms = []
         for n_v, n_h in zip(layer_units[:-1], layer_units[1:]):
-            rbm = RbmNetwork(n_v, n_h)
+            rbm = RbmNetwork(n_v, n_h, n_sampling_steps=k)
             self.rbms.append(rbm)
 
     def test_trial(self):
@@ -45,23 +48,60 @@ class DBN(Network):
         greedy_layerwise_training()
         up_down_training()
 
-    def greedy_layerwise_training(self, wholeds):
-        layer_input = wholeds
-        for rbm in self.rbms:
-            rbm.train(layer_input)
-            layer_input = rbm.propagatefwd(layer_input)
-            
-    def up_pass(self, ):
+    def train_greedy(self, dataset):
+        pass
+         
+    def train_backfiting(self, patterns):    
+        for _ in range(n_epochs):
+            vis_states = grab_minibatch(patterns, self.n_in_minibatch)        
+             # obtain intial values from bottom rbm
+            vis_act = self.rbms[0].we_dont_know_what_comes_here(vis_states)
+            # start backfitting
+            _, top_state = up_pass(vis_states, vis_act)
+            _, v_k_state = run_cdk_on_top_layer(top_state)
+            down_pass(v_k_state)
+                    
+    def up_pass(self, h_0_act, h_0_state):
+        """
+        Performs an up pass on the data and performs mean-field updates of
+        generative weights
+        """
+        for rbm in self.rbms[:-1]: # skip last RBM, we'll do CD-k there
+            # compute forward activation
+            h_1_act = rbm.propagate_fwd(h_0_state)
+            h_1_state = rbm.samplestates(h_1_act)
+            # perform generative weight update
+            delta_w = self.lrate * h_1_state * (h_0_state - h_0_act)
+            rbm.update_weights_with_delta(delta_w)
+            # feed activations into higher-level RBM
+            h_0_act = h_1_act
+            h_0_state = h_1_state
 
-    def down_pass(self, ):
+        return h_1_act, h_1_state
+
+
+    def down_pass(self, h_1_act, h_1_state):
+        """
+        Performs a downward pass on the data and performs mean-field updates of
+        the network's recognition weights
+        """
+        for rbm in reversed(self.rbms[:-1]): # go from (top layer - 1) to 0
+            # compute backward activation
+            h_0_act = rbm.propagate_back(h_1_state)
+            h_0_state = rbm.samplestates(h_0_act)
+            # perform recognition weight update
+            delta_w = self.lrate * h_0_state * (h_1_state - h_1_act)
+            rbm.update_weights_with_delta(delta_w)
+            h_1_act = h_0_act
+            h_1_state = h_0_state
+        return h_0_act, h_0_state
 
     def run_cdk_on_top_layers(self, v_plus):
         # get v_plus 
         top_rbm = self.rbms[-1]
-        v_minus = top_rbm.learn_trial(v_plus) # v_minus after k-step CD
-        return v_minus
-            
+        v_k_act, v_k_state = top_rbm.k_gibbs_steps(self, v_plus) # v_minus after k-step CD
+        return v_k_act, v_k_state
         
-
+if __mame__ == "__main__":
     
     

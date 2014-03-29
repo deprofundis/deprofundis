da786178736ab0102055b50902cb5f2e0300fe51
David Haber <david.haber90@gmail.com>
2014-03-29 16:01:55 +0100

    Implemented dynamic distribution for CRBM model

--- models/distribution.py
+++ models/distribution.py
@@ -155,6 +155,122 @@ class Bernoulli(Distribution):
 
     def get_distribution_type(self):
         return Distribution.Type.DISCRETE
+        
+class DynamicBernoulli(Distribution):
+    """
+    Implements a distribution with dynamic biases
+    """
+    def __init__(self, size_visible, size_hidden, weights=None, vis_vis_weights=None, 
+                 vis_hid_weights=None, bias_hidden=None, bias_visible=None,
+                 m_lag_visible=1, n_lag_hidden=1):
+        Distribution.__init__(self, size_visible, size_hidden, weights, bias_hidden, bias_visible)
+        
+        # dynamic biases
+        self.m_lag_visible = m_lag_visible # m
+        self.n_lag_hidden = n_lag_hidden # n
+        
+        # initialize weights
+        if vis_vis_weights is None: # A
+            self.vis_vis_weights = np.random.uniform(-4 * np.sqrt(6. / (size_visible + size_visible)),
+                                             4 * np.sqrt(6. / (size_visible + size_visible)),
+                                             size=(size_visible, size_visible, m_lag_visible)) # TODO initialization
+        else:
+            assert (vis_vis_weights.shape == (size_visible, size_visible, m_lag_visible))
+            self.vis_vis_weights = np.copy(vis_vis_weights)
+        
+        if vis_hid_weights is None: # B
+            self.vis_hid_weights = np.random.uniform(-4 * np.sqrt(6. / (size_hidden + size_visible)),
+                                             4 * np.sqrt(6. / (size_hidden + size_visible)),
+                                             size=(size_visible, size_hidden, n_lag_hidden)) # TODO initialization
+        else:
+            assert (vis_hid_weights.shape == (size_visible, size_hidden, n_lag_hidden))
+            self.vis_hid_weights = np.copy(vis_hid_weights)
+            
+            
+    def score_energy(self, visible, hidden):
+        """
+        @param visible: visible input (includes time-lagged values)
+        @param hidden: hidden input (NO time-lagged values)
+        """
+        lag = max(self.n_lag_hidden, self.m_lag_visible)
+        assert(visible.shape == (lag, self.size_visible))
+        
+        
+    def conditional_prob_v(self, hidden, visible_lagged):
+        """
+        @param hidden: hidden input
+        @param visible_lagged: lagged visible input (m input vectors)
+        """
+        assert(hidden.shape == (self.size_hidden))
+        assert(visible_lagged.shape == (self.m_lag_visible, self.size_visible))
+        
+        # calculate fan in to visible units
+        fan_in = self.bias_visible
+        for lag in range(self.m_lag_visible):
+            fan_in += np.dot(visible_lagged[lag, :], self.vis_vis_weights[:,:,lag])
+
+        fan_in += np.dot(self.weights, hidden)
+
+        act = act_fn.sigmoid(fan_in)
+        # returns a matrix of size (1xself.size_visible)
+        return act
+
+    def conditional_prob_h(self, visible, visible_lagged):
+        """
+        @param visible: visible input
+        @param visible_lagged: lagged visible input(n input vectors)
+        """
+        assert(visible.shape == (self.size_visible))
+        assert(visible_lagged.shape == (self.n_lag_hidden, self.size_visible))
+        
+        # calculate fan in to hidden units
+        fan_in = self.bias_hidden
+        for lag in range(self.n_lag_hidden):
+            fan_in += np.dot(visible_lagged[lag, :], self.vis_hid_weights[:,:,lag])
+        
+        fan_in += np.dot(visible, self.weights)
+        
+        act = act_fn.sigmoid(fan_in)
+        # returns a matrix of size (1xself.size_hidden)
+        return act
+
+    def state_v(self, hidden, visible_lagged):
+        """
+        @param hidden: hidden input
+        @param visible_lagged: lagged visible input (m input vectors)
+        """
+        assert(hidden.shape == (self.size_hidden))
+        assert(visible_lagged.shape == (self.m_lag_visible, self.size_visible))
+        
+        # compute conditional probabilities of visible units
+        conditional_prob_v = self.conditional_prob_v(hidden, visible_lagged)
+        # sample states
+        state_v = conditional_prob_v > np.random.uniform(0,1,size=(self.size_visible))
+        # returns a matrix (size of batch=1) x (size of visible layer) # TODO include minibatches
+        return conditional_prob_v, state_v.astype(int)
+        
+    def state_h(self, visible, visible_lagged):
+        """
+        @param visible: visible input
+        @param visible_lagged: lagged visible input(n input vectors)
+        """
+        assert(visible.shape == (self.size_visible))
+        assert(visible_lagged.shape == (self.n_lag_hidden, self.size_visible))
+        
+        # compute conditional probabilities of hidden units
+        conditional_prob_h = self.conditional_prob_h(visible)
+        # sample states
+        state_h = conditional_prob_h > np.random.uniform(0,1,size=(self.size_hidden))
+        # returns a matrix (size of batch=1) x (size of hidden layer) # TODO include minibatches
+        return conditional_prob_h, state_h.astype(int)
+        
+    def free_energy(self, visible, hidden):
+        raise NotImplementedError("Not implemented")
+        
+
+    def get_distribution_type(self):
+        return Distribution.Type.DISCRETE
+
 
 
 # TODO Implement variance as parameter into distribution

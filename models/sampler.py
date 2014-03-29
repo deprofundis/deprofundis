6c9f653826e828fc422395d261fcc7d54e1360e4
David Haber <david.haber90@gmail.com>
2014-03-29 19:13:12 +0100

    Added dynamic block Gibbs sampler (DBGS) for CRBM implementation

--- models/sampler.py
+++ models/sampler.py
@@ -21,7 +21,6 @@ class BlockGibbsSampler(Sampler):
     """
     def __init__(self, model_distribution, sampling_steps=1):
         Sampler.__init__(self, model_distribution)
-
         self.sampling_steps = sampling_steps
 
     def sample(self, visible_0_state):
@@ -41,3 +40,32 @@ class BlockGibbsSampler(Sampler):
         return hidden_0_prob, hidden_0_state,\
                hidden_k_prob, hidden_k_state, \
                visible_k_prob, visible_k_state
+               
+class DynamicGibbsSampler(Sampler):
+    """
+    Implements a Dynamic Block Gibbs Sampler (DBGS)
+    """
+    def __init__(self, model_distribution, sampling_steps=1):
+        Sampler.__init__(self, model_distribution)
+        self.sampling_steps = sampling_steps
+        
+    def sample(self, visible_0_state, visible_lagged):
+        # assert that the initial data sample has the correct shape
+        assert (self.model_distribution.size_visible in visible_0_state.shape)
+        assert (len(visible_0_state) == len(visible_lagged))
+        
+        # calculate the initial state for the hidden unit
+        hidden_0_prob, hidden_0_state = self.model_distribution.state_h(visible_0_state, visible_lagged)
+        # copy the value since a numpy array is a mutable object, there it will get changed during the sampling process
+        # which we don't want
+        visible_k_state = np.copy(visible_0_state)
+        hidden_k_state = np.copy(hidden_0_state)
+        # start block gibbs sampling procedure
+        for step in range(self.sampling_steps):
+            visible_k_prob, visible_k_state = self.model_distribution.state_v(hidden_k_state, visible_lagged)
+            hidden_k_prob, hidden_k_state = self.model_distribution.state_h(visible_k_state, visible_lagged)
+
+        return hidden_0_prob, hidden_0_state,\
+               hidden_k_prob, hidden_k_state, \
+               visible_k_prob, visible_k_state
+        
